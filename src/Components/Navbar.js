3a893a06c62ba25864570e2c1152ec16acaa1fa7
Joy Wambui <joy.wambui@student.moringaschool.com>
2024-08-13 12:29:55 +0300

    Add app

--- src/Components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Navbar() {
-  return (
-    <form>Navbar</form>
-  )
-}
-
-export default Navbar
--- src/Components/Table.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Table() {
-  return (
-    <form>home</form>
-  )
-}
-
-export default Table

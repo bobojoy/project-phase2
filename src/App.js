2a5c8a8286635ad63beca1852429d1d1788f1472
charles <charles.angila@student.moringaschool.com>
2024-08-14 15:11:38 +0300

    routes.js-implemented paths for routes,modified index.js for routing,Created NavBar component for linking the navigation bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1 className='text-red'>Projo</h1>
+          <h1 className="text-red">Projo</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -19,7 +19,6 @@ function App() {
           Learn React
         </a>
       </header>
-    
     </div>
   );
 }

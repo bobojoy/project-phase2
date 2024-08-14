d6a324d1172ac4c0d165e33766efbd07a35a1039
Joy Wambui <joy.wambui@student.moringaschool.com>
2024-08-14 11:31:08 +0300

    Added the home component and some styling on tailwind

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1 className='text-red-500'>Projo</h1>
+          <h1 className='text-red'>Projo</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
